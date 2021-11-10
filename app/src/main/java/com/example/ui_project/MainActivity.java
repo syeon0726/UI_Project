21d78db101c222c847c78381eefa26f611216b50
sons369@soongsil.ac.kr <sons369@soongsil.ac.kr>
2021-11-10 22:58:49 +0900

    로그아웃 기능 구현, 구글 로그인기능 미완성..

--- app/src/main/java/com/example/ui_project/MainActivity.java
+++ app/src/main/java/com/example/ui_project/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.ui_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogout;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout=findViewById(R.id.logout);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
